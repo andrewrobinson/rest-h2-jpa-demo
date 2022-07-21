3f158e9ad725533214e422c3785664ffdde3ad40
Andrew Robinson <andrewmcrobinson@gmail.com>
2022-07-21 23:53:44 +0100

    wip

--- src/main/java/com/andrewrobinson/resth2jpademo/controllers/PredictionController.java
+++ src/main/java/com/andrewrobinson/resth2jpademo/controllers/PredictionController.java
@@ -7,21 +7,31 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class PredictionController {
 
-//    @GetMapping("/predictions/")
-//    List<Subscription> getPrediction() {
-//
-//        return repository.findAll();
-//    }
+        //https://www.section.io/engineering-education/implement-rate-limiting-in-spring-boot/
+
+    @GetMapping("/predictions/")
+    List<Integer> getPrediction() {
+
+        //TODO - stock ticker in url and duration from query param
+        //TODO - stock ticker http call
+        //TODO - look up subscription of logged in user
+        //TODO - rate limiter
+
+        var results = new ArrayList<Integer>();
+        results.add(1);
+        results.add(2);
+        results.add(3);
+        results.add(7);
+        return results;
+    }
 
     private String getLoggedInUser() {
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//        String currentPrincipalName = authentication.getName();
-//        return currentPrincipalName;
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (!(authentication instanceof AnonymousAuthenticationToken)) {
